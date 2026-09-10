28fa10addf0c7cc8dedd9b90f1f0a14b75bba532
guru340 <gurusangwani06@gmail.com>
2026-09-10 19:04:08 +0530

    Project Initalisation

---
+++ src/main/java/com/example/Meeting_Notes_Summariser/MeetingNotesSummariserApplication.java
@@ -0,0 +1,13 @@
+package com.example.Meeting_Notes_Summariser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingNotesSummariserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingNotesSummariserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Meeting_Notes_Summariser/MeetingNotesSummariserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Meeting_Notes_Summariser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingNotesSummariserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
