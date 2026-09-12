bf524447d7d549d2acd8010ccf3a96ad0d783d08
guru340 <gurusangwani06@gmail.com>
2026-09-13 02:21:04 +0530

    Huggingface Implementation Completed

--- src/main/java/com/example/Meeting_Notes_Summariser/config/AIProviderConfig.java
+++ src/main/java/com/example/Meeting_Notes_Summariser/config/AIProviderConfig.java
@@ -1,6 +1,7 @@
 package com.example.Meeting_Notes_Summariser.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.huggingface.HuggingfaceChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,10 @@ public class AIProviderConfig {
     @Bean("openAIChatClient")
     ChatClient openAIchatClient(OpenAiChatModel openAiChatModel){
         return ChatClient.builder(openAiChatModel).build();
+    }
+
+    @Bean("huggingfaceChatClient")
+    ChatClient huggingfaceChatClient(HuggingfaceChatModel huggingfaceChatModel){
+        return ChatClient.builder(huggingfaceChatModel).build();
     }
 }
---
+++ src/main/java/com/example/Meeting_Notes_Summariser/controller/huggingfaceController.java
@@ -0,0 +1,31 @@
+package com.example.Meeting_Notes_Summariser.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/huggingface/chat")
+public class huggingfaceController {
+
+    private final static String SYSTEM_PROPMT="You are a senior engineer. Generate code based on the given description. "+
+            "Ensure the code is idiomatic, efficient, and follows best pracices.";
+
+    private final ChatClient chatClient;
+
+    public huggingfaceController(@Qualifier("huggingfaceChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @PostMapping("/generate-code")
+    public ChatClientResponse generatecode(@RequestBody String message){
+        return chatClient.prompt()
+                .system(SYSTEM_PROPMT).
+                user(message)
+                .call().chatClientResponse();
+    }
+}
