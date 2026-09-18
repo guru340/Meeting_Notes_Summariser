3a3b3b1b10f5827ccbcea266d51a33a5fac59f84
guru340 <gurusangwani06@gmail.com>
2026-09-18 23:26:26 +0530

    Added   Controller For Generating Strucutred Output

---
+++ src/main/java/com/example/Meeting_Notes_Summariser/controller/OpenAIStructuredOutputController.java
@@ -0,0 +1,41 @@
+package com.example.Meeting_Notes_Summariser.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/openai/chat")
+public class OpenAIStructuredOutputController {
+
+    private final  ChatClient chatclient;
+
+    public OpenAIStructuredOutputController(@Qualifier("openAIChatClient") ChatClient chatclient) {
+        this.chatclient = chatclient;
+    }
+
+    @PostMapping("/structured-list")
+    public List<String> liststructured(@RequestBody String message){
+        return chatclient.prompt()
+                .user(message)
+                .call()
+                .entity(new ListOutputConverter());
+    }
+
+    @PostMapping("/structured-map")
+    public Map<String,Object> structuredmap(@RequestBody String message){
+        return chatclient.prompt()
+                .user(message)
+                .call()
+                .entity(new MapOutputConverter());
+    }
+
+}
