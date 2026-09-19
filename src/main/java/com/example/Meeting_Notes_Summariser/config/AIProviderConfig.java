6ba1bd2fc908aadca4abbf342bd8e2c043b26277
guru340 <gurusangwani06@gmail.com>
2026-09-19 14:44:10 +0530

    Added SafeguardAdvisor

--- src/main/java/com/example/Meeting_Notes_Summariser/config/AIProviderConfig.java
+++ src/main/java/com/example/Meeting_Notes_Summariser/config/AIProviderConfig.java
@@ -1,20 +1,23 @@
 package com.example.Meeting_Notes_Summariser.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.huggingface.HuggingfaceChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class AIProviderConfig {
 
     @Bean("openAIChatClient")
     ChatClient openAIchatClient(OpenAiChatModel openAiChatModel
-    ,SimpleLoggerAdvisor simpleLoggerAdvisor){
+    ,SimpleLoggerAdvisor simpleLoggerAdvisor,SafeGuardAdvisor safeGuardAdvisor){
 
-        return ChatClient.builder(openAiChatModel).defaultAdvisors(simpleLoggerAdvisor).build();
+        return ChatClient.builder(openAiChatModel).defaultAdvisors(safeGuardAdvisor,simpleLoggerAdvisor).build();
     }
 
 //    @Bean("huggingfaceChatClient")
@@ -25,5 +28,12 @@ public class AIProviderConfig {
     @Bean
     SimpleLoggerAdvisor simpleLoggerAdvisor(){
         return new SimpleLoggerAdvisor();
+    }
+
+    @Bean
+    SafeGuardAdvisor safeGuardAdvisor(){
+        return new SafeGuardAdvisor(
+                List.of("password","ssn","credit card","hack")
+        );
     }
 }
